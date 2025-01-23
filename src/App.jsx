d8341c38de1d3b1e2cefe2af7ea5d6e3c11f146f
Surya <suryadantuluri2005@gmail.com>
2025-01-24 00:15:09 +0530

    Home Updated

--- src/App.jsx
+++
@@ -1,8 +0,0 @@
-function App() {
-  return (
-    <div className="App">
-      <h1 className="App-heading">Event Management System</h1>
-    </div>
-  )
-}
-export default App;
---
+++ src/Users/Home.jsx
@@ -0,0 +1,46 @@
+import './Home.css';
+function Home() {
+    return (
+        <div className="Home">
+            <h1 className="Home-heading">Event Management System</h1>
+            <div className="maindiv">
+                <div className="content">
+                    <div className="contentimg"><img src="https://static.vecteezy.com/system/resources/thumbnails/024/669/489/small_2x/mountain-countryside-landscape-at-sunset-dramatic-sky-over-a-distant-valley-green-fields-and-trees-on-hill-beautiful-natural-landscapes-of-the-carpathians-generative-ai-variation-5-photo.jpeg" alt="" /></div>
+
+                    <h2 className="contenth1">Shedule</h2>
+
+                    <div className="contenttext">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corporis magnam laborum hic maxime officiis esse delectus </div>
+                </div>
+                <div className="content">
+                    <div className="contentimg"><img src="https://static.vecteezy.com/system/resources/thumbnails/024/669/489/small_2x/mountain-countryside-landscape-at-sunset-dramatic-sky-over-a-distant-valley-green-fields-and-trees-on-hill-beautiful-natural-landscapes-of-the-carpathians-generative-ai-variation-5-photo.jpeg" alt="" /></div>
+
+                    <h2 className="contenth1">Shedule</h2>
+
+                    <div className="contenttext">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corporis magnam laborum hic maxime officiis esse delectus </div>
+                </div>
+                <div className="content">
+                    <div className="contentimg"><img src="https://static.vecteezy.com/system/resources/thumbnails/024/669/489/small_2x/mountain-countryside-landscape-at-sunset-dramatic-sky-over-a-distant-valley-green-fields-and-trees-on-hill-beautiful-natural-landscapes-of-the-carpathians-generative-ai-variation-5-photo.jpeg" alt="" /></div>
+                    
+                    <h2 className="contenth1">Shedule</h2>
+
+                    <div className="contenttext">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corporis magnam laborum hic maxime officiis esse delectus </div>
+                </div>
+                <div className="content">
+                    <div className="contentimg"><img src="https://static.vecteezy.com/system/resources/thumbnails/024/669/489/small_2x/mountain-countryside-landscape-at-sunset-dramatic-sky-over-a-distant-valley-green-fields-and-trees-on-hill-beautiful-natural-landscapes-of-the-carpathians-generative-ai-variation-5-photo.jpeg" alt="" /></div>
+                    
+                    <h2 className="contenth1">Shedule</h2>
+
+                    <div className="contenttext">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corporis magnam laborum hic maxime officiis esse delectus </div>
+                </div>
+                <div className="content">
+                    <div className="contentimg"><img src="https://static.vecteezy.com/system/resources/thumbnails/024/669/489/small_2x/mountain-countryside-landscape-at-sunset-dramatic-sky-over-a-distant-valley-green-fields-and-trees-on-hill-beautiful-natural-landscapes-of-the-carpathians-generative-ai-variation-5-photo.jpeg" alt="" /></div>
+                    
+                    <h2 className="contenth1">Shedule</h2>
+
+                    <div className="contenttext">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corporis magnam laborum hic maxime officiis esse delectus </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+export default Home;
