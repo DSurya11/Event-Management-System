4e8d0e45a1e3bad084f3c88195de280ff2c88855
Surya <suryadantuluri2005@gmail.com>
2025-01-26 11:38:23 +0530

    new

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,13 @@ import './index.css'
 import Navbar from './Components/Navbar.jsx'
 import Footer from './Components/Footer.jsx'
 
+import Home from './Users/Home.jsx'
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Navbar/>
+    <Home />
     <Footer />
+    
   </StrictMode>
 )
