3a6cbdb6a317c4631b3b708a6970c91f1b75d1fa
RishiIRL <rishindrakapilc@gmail.com>
2025-01-26 11:37:10 +0530

    Colors

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import Navbar from './Components/Navbar.jsx'
 import Footer from './Components/Footer.jsx'
+import Home from './Users/Home.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Navbar/>
+    <Home/>
     <Footer />
   </StrictMode>
 )
