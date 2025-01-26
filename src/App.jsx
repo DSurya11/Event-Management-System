bca7f6e0e737d59116a545e7fed6ed8589f34364
RishiIRL <rishindrakapilc@gmail.com>
2025-01-26 13:37:05 +0530

    Scroll updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
+import Browse from './Users/Browse.jsx'
 import Home from './Users/Home.jsx'
 function App() {
     return (
         <div className='Main'>
-            <Home></Home>
+            <Home/>
         </div>
     )
 }
