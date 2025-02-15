cd9f82a8f362f33fc22b36acaf69039fae24a621
Surya <suryadantuluri2005@gmail.com>
2025-02-15 21:22:53 +0530

    new1

--- src/main.jsx
+++ src/main.jsx
@@ -8,8 +8,9 @@ import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Navbar/>
-    <App/>
+
+    <Navbar />
+    <App />
     <Footer />
 
   </StrictMode>
