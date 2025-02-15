b493159415cdd61d4803910f703faa05d71bc6af
Surya <suryadantuluri2005@gmail.com>
2025-02-15 21:07:14 +0530

    new

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,11 @@ import './index.css'
 import Navbar from './Components/Navbar.jsx'
 import Footer from './Components/Footer.jsx'
 import App from './App.jsx'
+import Admin from './Admin/Admin.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Navbar/>
-    <App/>
-    <Footer />
-
+    <Admin/>
+    
   </StrictMode>
 )
