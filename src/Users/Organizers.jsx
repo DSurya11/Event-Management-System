cc1ba6e15257560465aa340a52704fcaf6a0fca8
kvpradeep279 <kollabcs136@gmail.com>
2025-08-01 17:15:10 +0530

    New changes insertion

--- src/Users/Organizers.jsx
+++ src/Users/Organizers.jsx
@@ -3,9 +3,7 @@ import './Organizers.css';
 function Organizers() {
     return (
         <div className='organizers_browse'>
-            <h1>Organizers</h1>
-            <div className='Sports'>
-            <h2>Sports</h2>
+            <h2>Organizers</h2>
             <div className='Org'>
                 <div className='Event'>
                     <img src=".\public\white.jpeg" alt="" />
@@ -15,49 +13,11 @@ function Organizers() {
                     <img src=".\public\white.jpeg" alt="" />
                     <h3>Club_Name</h3>
                 </div>
-            </div>
-            </div>
-            <div className='Technical'>
-            <h2>Technical</h2>
-            <div className='Org'> 
-                <div className='Event'>
-                    <img src=".\public\white.jpeg" alt="" />
-                    <h3>Club_Name</h3>
-                </div>
                 <div className='Event'>
                     <img src=".\public\white.jpeg" alt="" />
                     <h3>Club_Name</h3>
                 </div>
             </div>
-            </div>
-            
-           <div className='Acadamic'>
-           <h2>Acadacmic</h2>
-            <div className='Org'>
-                <div className='Event'>
-                    <img src=".\public\white.jpeg" alt="" />
-                    <h3>Club_Name</h3>
-                </div>
-                <div className='Event'>
-                    <img src=".\public\white.jpeg" alt="" />
-                    <h3>Club_Name</h3>
-                </div>
-            </div>
-           </div>
-           <div className='Cultural'>
-           <h2>Cultural</h2>
-            <div className='Org'>
-                <div className='Event'>
-                    <img src=".\public\white.jpeg" alt="" />
-                    <h3>Club_Name</h3>
-                </div>
-                <div className='Event'>
-                    <img src=".\public\white.jpeg" alt="" />
-                    <h3>Club_Name</h3>
-                </div>
-            </div>
-           </div>
-
 
         </div>
     )
