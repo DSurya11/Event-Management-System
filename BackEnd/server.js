44fd3b7613ee15c909ea0173174fe29331d56014
kvpradeep279 <kollabcs136@gmail.com>
2025-08-02 10:08:27 +0530

    Css improvements

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -1090,8 +1090,3 @@ app.use('/uploads', express.static('uploads'));
 
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-
-
-
-const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
